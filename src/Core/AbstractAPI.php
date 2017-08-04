5f2b472063687e2d20ad005618ee07bf771bb94d
hao <xiaohao@haosblog.com>
2017-08-04 18:38:41 +0800

    new package

---
+++ src/Core/AbstractAPI.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * 豆瓣SDK抽象基类
+ *
+ * @create 2017-7-31 17:21:31
+ * @author hao
+ */
+
+namespace Haosblog\Douban\Core;
+
+use InvalidArgumentException;
+
+use GuzzleHttp\Client;
+
+class AbstractAPI
+{
+
+    /**
+     * GET request.
+     *
+     * @param string $url
+     * @param array  $options
+     *
+     * @return ResponseInterface
+     *
+     * @throws HttpException
+     */
+    public function get($url, array $options = [])
+    {
+        return $this->request($url, 'GET', ['query' => $options]);
+    }
+
+    /**
+     * POST request.
+     *
+     * @param string       $url
+     * @param array|string $options
+     *
+     * @return ResponseInterface
+     *
+     * @throws HttpException
+     */
+    public function post($url, $options = [])
+    {
+        $key = is_array($options) ? 'form_params' : 'body';
+
+        return $this->request($url, 'POST', [$key => $options]);
+    }
+    
+    /**
+     * 请求api
+     * 
+     * @param type $url
+     * @param type $method
+     * @param type $options
+     * @return type
+     */
+    protected function request($url, $method = 'GET', $options = [])
+    {
+        $method = strtoupper($method);
+        $response = (new Client())->request($method, $url, $options);
+        
+        $content = $response->getBody();
+
+        return $content;
+    }
+    
+}
